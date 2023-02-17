c16f96ffa512e4a1ca8b4c012025ea98ef78f39f
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-16 18:37:01 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Ac prgam that prints a line to thee stadard error
+ * Retun:1 (pass)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
