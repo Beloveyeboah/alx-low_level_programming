3cb21fae373319d3179e3ef25ad19a3f50f2fedb
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-16 17:01:29 -0800

    upd 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A programme that prints puts function
- * Return: 0 (pass)
+ * main - A programme that prints a line with puts function
+ * Return: 0 (Pas)
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
