fc7935bef145d1da786631a5f6323199cd48d05a
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-16 16:22:13 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-{
-	puts(*\"Programming is like building a multilingual puzzle")
-		return(0)
+/**
+ * main - A programme that prints puts function
+ * Return: 0 (pass)
+ */
+int main(void)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
