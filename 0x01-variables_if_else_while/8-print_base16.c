fba45b1853dbea1c7b4ae7a9d62c9494be69ba05
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-17 20:34:59 -0800

    last

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Entry point
+ * description: base
+ * Return:0
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+
+	while
+		(d < 10)
+		{
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f')
+		{
+			putchar(c);
+			c++;
+		}
+	return (0);
+}
+
