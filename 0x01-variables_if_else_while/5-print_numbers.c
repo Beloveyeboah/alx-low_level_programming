887b63eda78bf7688e73bd308f977af7f39ecec7
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-17 18:43:30 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * description: single digits
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
+
