80e55adb357ca369e81ec41cfe9ae3b6f84747cb
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-21 17:25:59 -0800

    re ./2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (i <= 9)
+	while (i < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			i++;
+			c++;
 		}
+		_putchar('\n');
+		i++;
 	}
 }
