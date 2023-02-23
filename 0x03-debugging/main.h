cbdacfbc40cdc1550f084f4669eb8b562b8a9c2b
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-23 02:05:21 -0800

    mainv

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,4 @@ int largest_or_negative(int a,int b,int c);
 void print_remaining_days(int month,int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
-
-#endif
+#endif /* MAIN_H */
