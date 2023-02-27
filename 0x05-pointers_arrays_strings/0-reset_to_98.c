9f16e08422395391ae3057e35613da7e241e62e1
osei yeboah isaac <beloveyeboah@gmail.com>
2023-02-27 08:00:21 -0800

    98 reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - a function that takes pointer to an int
+ *
+ * @n: input
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+ void reset_to_98(int *n);
+	
+#endif
