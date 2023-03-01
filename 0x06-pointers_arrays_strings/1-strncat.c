b9f8217fd655e594fd98662ea97fdabcc70d843a
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-01 15:40:31 -0800

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,6 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -27,5 +28,5 @@ char *_strncat(char *dest, char *src, int n)
 		j++;
 	}
 	dest[i] = '\0';
-	return (0);
+	return (dest);
 }
