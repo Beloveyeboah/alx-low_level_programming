429fb2694a5ac18aaad521934a4ba3e3e5abc396
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-02 14:05:02 -0800

    /7-1337

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
  * string_toupper - string_toupper change loercase to
  *
  * @n: pointer to char
  *
- * Return: 0
+ * Return: char
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 	int i;
 
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' & n[i]  'z')
-			n[i] = n[i] - 22;
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
 			i++;
 	}
 	return (n);
