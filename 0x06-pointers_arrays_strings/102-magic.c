089df044c79e29ac26c066f10079320e53bb5e18
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-03 03:07:33 -0800

    5-string_toupper

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - main function wrires
+ * Return: 0 (pass)
+ */
 int main(void)
 {
 	int n;
@@ -12,8 +15,7 @@ int main(void)
  * Remember:
  * - you are not allowed to use a
  * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
+ * - than this line of code
  */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
