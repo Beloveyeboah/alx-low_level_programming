15f38f953a49903b821c6f390379b89fecc85619
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-05 10:03:15 -0800

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - set_string function sets
+ *
+ * @to: the char
+ *
+ * @s: the pointdr
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
