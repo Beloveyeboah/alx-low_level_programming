279dbbefcd7fa71d5bf427346f8abf207df34288
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-08 14:07:10 -0800

    6-prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int actual_prime(int n, int i)
-
 /**
  * is_prime_number - is_prime_number function write
  *
@@ -15,6 +13,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
+	else
 		return (actual_prime(n, n - 1));
 }
 
@@ -31,8 +30,9 @@ int actual_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
-	if (n % i == 0 && i > 0)
+	else if (n % i == 0 && i > 0)
 		return (0);
+	else
 		return (actual_prime(n, i - 1));
 }
 
