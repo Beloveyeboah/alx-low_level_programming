af3309932c9779c3ff6b87948df439aa608fd490
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-14 08:56:36 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_array - create_array prtints
+ * @size: number
+ * @c: character
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	buffer = (char *) malloc(size * sizeof(c));
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+	}
+	while (position < size)
+	{
+		*(buffer + position)  = c;
+		position++;
+	}
+	return (buffer);
+}
