82a6c640deff320d85ae2990452cd4ef767c149b
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-16 12:24:18 -0700

    100-realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _realloc - _realloc paste
+ * @ptr: pointer
+ * @old_size: the
+ * @new_size: the
+ * Return: 1
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *clone, *realloc;
+	unsigned int i;
+
+	if (ptr != NULL)
+		clone = ptr;
+	else
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+	}
+	realloc = malloc(new_size);
+	if (realloc == NULL)
+		return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(realloc + 1) = clone[i];
+	}
+	free(ptr);
+return (realloc);
+}
