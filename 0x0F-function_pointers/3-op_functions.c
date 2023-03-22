9ffd89b6b5e2f45e567c1aab591640dc1a74f47b
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-22 11:23:02 -0700

    calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - op_add(
+ * @a: the
+ * @b: the
+ *
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - op_sub
+ * @a: the
+ * @b: the
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - op_mul
+ * @a: the
+ * @b: the
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - op_div
+ * @a: the
+ * @b: the
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - op_mod
+ * @a: the
+ * @b: the
+ * Return: 1
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
