b37cb3e056646a2901a963e85ad737bf3f7110bd
osei yeboah isaac <beloveyeboah@gmail.com>
2023-03-30 17:01:43 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-void _attribute_((constructor)) hare(void);
+void first(void) __attribute__ ((constructor));
 
 /**
- * hare - prints string
- *
+ * first - first prints
  * Return: nothing
  */
-void hare(void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
