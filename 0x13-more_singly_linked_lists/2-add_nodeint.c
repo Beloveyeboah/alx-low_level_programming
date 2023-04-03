91f7da0383c083262fe720e9ee394ca91c5dd883
osei yeboah isaac <beloveyeboah@gmail.com>
2023-04-03 16:03:15 -0700

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add_nodeint adds all nodes
+ *
+ * @head: pointer
+ *
+ * @n: interger
+ *
+ * Return: 0
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *sun;
+
+	sun = malloc(sizeof(listint_t));
+	if (sun == NULL)
+		return (NULL);
+	sun->n = n;
+	sun->next = *head;
+	return (sun);
+}
