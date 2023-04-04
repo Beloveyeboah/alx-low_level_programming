2478384b800a4c46cf5220b19ccac564f8949e37
osei yeboah isaac <beloveyeboah@gmail.com>
2023-04-04 05:55:23 -0700

    9 rool

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert_nodeint_at_index( write
+ *
+ * @head: pointer
+ * @n: the interger
+ *
+ * @idx: the next
+ *
+ * Return: 0
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *sun, *cat = *head;
+	unsigned int fun;
+
+	sun = malloc(sizeof(listint_t));
+	if (sun == NULL)
+		return (NULL);
+	sun->n = n;
+	if ((idx == 0))
+	{
+		sun->next = cat;
+		*head = sun;
+	}
+	for (fun = 0; fun < (idx - 1); fun++)
+	{
+		if (cat == NULL || cat->next == NULL)
+			return (NULL);
+		cat = cat->next;
+	}
+	sun->next = cat->next;
+	cat->next = sun;
+	return (sun);
+}
