7f4c7cb827cb6e9b941b147566b39910021fc4e8
osei yeboah isaac <beloveyeboah@gmail.com>
2023-04-10 14:37:18 -0700

    1 file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		for (fun = 0; text_content[fun];)
 			fun++;
 	}
-	sun = open(filename, O_CREAT | O_RDWR, 0600);
+	sun = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	cat = write(sun, text_content, fun);
 	if (sun == -1 || cat == -1)
 		return (-1);
