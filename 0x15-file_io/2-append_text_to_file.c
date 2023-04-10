794b85990859b7aad8ad67a78d44891abbeaddca
osei yeboah isaac <beloveyeboah@gmail.com>
2023-04-10 14:24:00 -0700

    2 leett

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,4 +18,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	fun = 0;
 	if (filename == NULL)
 		return (-1);
-	if (text_content 
+	if (text_content != NULL)
+	{
+		for (fun = 0; text_content[fun];)
+			fun++;
+	}
+	m = open(filename, O_WRONLY | O_APPEND);
+	k = write(m, text_content, fun);
+	if (m == -1 || k == -1)
+		return (0);
+	close(m);
+	return (0);
+}
