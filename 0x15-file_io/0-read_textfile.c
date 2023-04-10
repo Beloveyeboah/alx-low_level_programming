0bcf3ad7247108c5e470d17884265dbac201322d
osei yeboah isaac <beloveyeboah@gmail.com>
2023-04-10 13:46:19 -0700

    file o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,23 +15,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t ak, mk, w;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 	omo = malloc(sizeof(char) * letters);
-	if (omo == NULL)
-	{
-		return (0);
-	}
 	ak = open(filename, O_RDONLY);
+	if (ak == -1)
+		return (0);
 	mk = read(ak, omo, letters);
 	w = write(STDOUT_FILENO, omo, mk);
-
-	if (ak == -1 || w == -1 || mk == -1 || w == mk)
-	{
-		free(omo);
-		return (0);
-	}
 	free(omo);
 	close(ak);
 	return (w);
