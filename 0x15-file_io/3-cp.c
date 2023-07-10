9090d1c14a69f437b5617e0542153def389351bf
beloveyeboah <beloveyeboah@gamil.com>
2023-07-10 14:17:02 -0700

    3-cp_file.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -78,14 +78,16 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file\n", argv[1]);
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
 			free(buf);
 			exit(98);
 		}
 		w = write(to, buf, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Erro: Can't write to\n", argv[2]);
+			dprintf(STDERR_FILENO,
+					"Erro: Can't write to %s\n", argv[2]);
 			free(buf);
 			exit(99);
 		}
