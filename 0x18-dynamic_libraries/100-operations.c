d6d9b6b5849654d3479a5877962c503fd06f04ce
beloveyeboah <beloveyeboah@gamil.com>
2023-08-21 15:00:43 -0700

    part

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,24 @@
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int mol(int a, int b)
+{
+	return (a % b);
+}
