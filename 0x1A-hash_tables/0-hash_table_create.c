0ab7c83dcde44b3e3eac7eda0d864fe9652e47c4
beloveyeboah <beloveyeboah@gamil.com>
2023-09-08 09:01:30 -0700

    new +

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int r = 0;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
+	ht->array = malloc(sizeof(hash_table_t) * size);
+
 	if (ht->array == NULL)
 	{
-		free(ht);
 		return (NULL);
 	}
+	while (r < size)
+	{
+		ht->array[r] = NULL;
+		r++;
+	}
+
 	return (ht);
 }
